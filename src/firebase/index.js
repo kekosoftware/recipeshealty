8c2f7e2535d58fd408614d4bc7cc79a6186b94e0
Gustavo Ariel <ariel@debianI5.debianI5>
2020-06-30 10:02:03 -0300

    Touches finally

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDQbOxjwbHhx3nfh3P_0FM4TZpDXSA_VS4",
-    authDomain: "meal-prep-2020.firebaseapp.com",
-    databaseURL: "https://meal-prep-2020.firebaseio.com",
-    projectId: "meal-prep-2020",
-    storageBucket: "meal-prep-2020.appspot.com",
-    messagingSenderId: "308302301572",
-    appId: "1:308302301572:web:0c3fa25230a3c63242b5d1",
-    measurementId: "G-BLV0RQFGGQ"
+    apiKey: "<yourApiKey>",
+    authDomain: "<yourAuthDomain>",
+    databaseURL: "<yourDatabaseURL>",
+    projectId: "<yourProjectID>",
+    storageBucket: "<yourStorageBucket>",
+    messagingSenderId: "<yourMessagingSenderID>",
+    appId: "<yourAppId>",
+    measurementId: "<yourMeasurementID>"
 };
 
 firebase.initializeApp(firebaseConfig);
